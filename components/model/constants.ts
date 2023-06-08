46585a57e039160e2ec7a20e1c857d0f82687f58
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2023-06-08 11:43:52 +0300

    feat: add clickable names

--- components/model/constants.ts
+++ components/model/constants.ts
@@ -32,3 +32,129 @@ export const circlePositions: {
     position: [CenterX - CIRCLE_OFFSET, CenterY, CenterZ - CIRCLE_OFFSET],
   },
 ]
+
+const clickableNames = [
+  "go_for_detox_LOD1",
+  "go_for_protection_LOD1.002",
+  "go_for_protection_LOD1.003",
+  "go_for_detox_LOD1.002",
+  "go_for_protection_LOD1.004",
+  "go_for_protection_LOD1.001",
+  "new_glow.001",
+  "happy age LOD1.002",
+  "new_repair",
+  "happy age LOD1.001",
+  "new_detox",
+  "new_glow.002",
+  "happy age LOD1.003",
+  "new_protection.001",
+  "new_repair.002",
+  "sculpt and glow LOD1",
+  "new_detox.002",
+  "new_glow.003",
+  "key",
+  "SM_guasha",
+  "lingette.002",
+  "étuilingette",
+  "lingette.001",
+] as const
+
+type Clickable = {
+  name: (typeof clickableNames)[number]
+  id: string
+}
+
+export const clickables: Clickable[] = [
+  {
+    name: "go_for_detox_LOD1",
+    id: "",
+  },
+  {
+    name: "go_for_protection_LOD1.002",
+    id: "",
+  },
+  {
+    name: "go_for_protection_LOD1.003",
+    id: "",
+  },
+  {
+    name: "go_for_detox_LOD1.002",
+    id: "",
+  },
+  {
+    name: "go_for_protection_LOD1.004",
+    id: "",
+  },
+  {
+    name: "go_for_protection_LOD1.001",
+    id: "",
+  },
+  {
+    name: "new_glow.001",
+    id: "",
+  },
+  {
+    name: "happy age LOD1.002",
+    id: "",
+  },
+  {
+    name: "new_repair",
+    id: "",
+  },
+  {
+    name: "happy age LOD1.001",
+    id: "",
+  },
+  {
+    name: "new_detox",
+    id: "",
+  },
+  {
+    name: "new_glow.002",
+    id: "",
+  },
+  {
+    name: "happy age LOD1.003",
+    id: "",
+  },
+  {
+    name: "new_protection.001",
+    id: "",
+  },
+  {
+    name: "new_repair.002",
+    id: "",
+  },
+  {
+    name: "sculpt and glow LOD1",
+    id: "",
+  },
+  {
+    name: "new_detox.002",
+    id: "",
+  },
+  {
+    name: "new_glow.003",
+    id: "",
+  },
+  {
+    name: "key",
+    id: "",
+  },
+  {
+    name: "SM_guasha",
+    id: "",
+  },
+  {
+    name: "lingette.002",
+    id: "",
+  },
+  {
+    name: "étuilingette",
+    id: "",
+  },
+  {
+    name: "lingette.001",
+    id: "",
+  },
+]
